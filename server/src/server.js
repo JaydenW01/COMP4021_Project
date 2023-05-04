a843b95724ec6641e2a5808ecd3726ecc89a1771
JaydenW01 <jaydenwong716hk@gmail.com>
2023-05-04 21:38:34 +0800

    Authorisation API DONE

---
+++ server/src/server.js
@@ -0,0 +1,146 @@
+const express = require("express");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const fs = require("fs");
+
+const app = express();
+app.use(express.static('public'));
+app.use(express.json());
+
+const bombermanSession = session({
+    secret : "grp28",
+    resave : false,
+    saveUninitialed : false,
+    rolling : true,
+    cookie: {maxAge: 99999999}
+});
+app.use(bombermanSession);
+
+function containWordCharsOnly(text) {
+    return /^\w+$/.test(text);
+}
+
+app.post("/register", (req, res) => {
+    // Get the JSON data from the body
+    const { username, displayName, password } = req.body;
+
+    //
+    // Reading the users.json file
+    //
+    const users = JSON.parse(fs.readFileSync("data/users.json"));
+    //
+    // Checking for the user data correctness
+    // check empty field
+    if (!username || !displayName || !password){
+        res.json({ status: "error", error: "There is empty field!" });
+        console.log(req.body)
+        return;
+    }
+    // check username format
+    if (!containWordCharsOnly(username)){
+        res.json({ status: "error", error: "The username should only contains underscores, letters or numbers" });
+        return;
+    }
+    // check if the username already exists
+    if (username in users){
+        res.json({ status: "error", error: "Username already exists" });
+        return;
+    }
+    //
+    // G. Adding the new user account
+    //
+    const hash = bcrypt.hashSync(password,10);
+    users[username] = {displayName,password:hash};
+    //
+    // H. Saving the users.json file
+    //
+    fs.writeFileSync("data/users.json",JSON.stringify(users,null," "));
+    //
+    // I. Sending a success response to the browser
+    //
+    res.json({status:"success"});
+
+    // Delete when appropriate
+    // res.json({ status: "error", error: "This endpoint is not yet implemented." });
+});
+
+// Handle the /signin endpoint
+app.post("/signin", (req, res) => {
+    // Get the JSON data from the body
+    const { username, password } = req.body;
+
+    //
+    // D. Reading the users.json file
+    //
+    const users = JSON.parse(fs.readFileSync("data/users.json"));
+    //
+    // E. Checking for username/password
+    //
+    if (!(username in users)){
+        res.json({status:"error",error:"Incorrect username/password"});
+        return;
+    }
+    const user = users[username];
+    if (!bcrypt.compareSync(password,user.password)){
+        res.json({status:"error",error:"Incorrect username/password"});
+        return;
+    }
+
+    //generate user session
+    req.session.user = {username,avatar:user.avatar,name:user.name};
+    //
+    // G. Sending a success response with the user account
+    //
+    res.json({status:"success", user:req.session.user});
+    // Delete when appropriate
+    // res.json({ status: "error", error: "This endpoint is not yet implemented." });
+});
+
+// Handle the /validate endpoint
+app.get("/validate", (req, res) => {
+    //
+    // B. Getting req.session.user
+    //
+    if (!req.session.user){
+        res.json({status:"error",error:"You have not signed in!"});
+        return;
+    }
+    //
+    // D. Sending a success response with the user account
+    //
+    res.json({status:"success", user:req.session.user});
+    // Delete when appropriate
+    // res.json({ status: "error", error: "This endpoint is not yet implemented." });
+});
+
+// Handle the /signout endpoint
+app.get("/signout", (req, res) => {
+
+    //
+    // Deleting req.session.user
+    //
+    if (req.session.user){
+        delete req.session.user;
+    }
+    //
+    // Sending a success response
+    //
+    res.json({status:"success"});
+    // Delete when appropriate
+    // res.json({ status: "error", error: "This endpoint is not yet implemented." });
+});
+
+
+
+
+
+// WebSocket codes ...
+const {createServer} = require("http");
+const {Server} = require("socket.io");
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+
+httpServer.listen(8000, () => {
+    console.log("The chat server has started at localhost 8000...");
+});
