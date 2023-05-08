edf1343dbf39b0f44600b1b417da7fd814a58236
JaydenW01 <jaydenwong716hk@gmail.com>
2023-05-08 22:04:15 +0800

    Fixed Player.js class module

--- server/src/Player.js
+++ server/src/Player.js
@@ -1,62 +1,55 @@
-const Player = (colour,playerNo) => {
-    let x = playerNo == 0 ? 0 : 20;
-    let y = playerNo == 0 ? 0 : 20;
-    let health = 3;
-    let cheat = false;
-    let ability = false;
+export default class Player {
+    constructor(colour,playerNo) {
+        this.colour = colour;
+        this.playerNo = playerNo;
+        this.x = playerNo == 0 ? 0 : 20;
+        this.y = playerNo == 0 ? 0 : 20;
+        this.health = 3;
+        this.cheat = 3;
+        this.ability = false;
+    }
     
     // the following function checks if the move is legal and return true/false, if true: update the coordinates
-    const moveUp = ()=>{ //TODO
+    moveUp = ()=>{ //TODO
         
     }
 
-    const moveDown = ()=>{ //TODO
+    moveDown = ()=>{ //TODO
 
     }
 
-    const moveLeft = ()=>{ //TODO
+    moveLeft = ()=>{ //TODO
         
     }
 
-    const moveRight = ()=>{ //TODO
+    moveRight = ()=>{ //TODO
         
     }
 
-    const reduceHealth = ()=>{
-        if (health-1 == 0){
+    reduceHealth = ()=>{
+        if (this.health-1 == 0){
             return false;
         } else {
-            health -= 1;
+            this.health -= 1;
             return true;
         }
     }
 
-    const increaseHealth = ()=>{
-        if (health==3){
+    increaseHealth = ()=>{
+        if (this.health==3){
             return
         } else {
-            health += 1;
+            this.health += 1;
         }
     }
 
-    const playerInfo = ()=>{
-        return {x,y,colour,health,ability,cheat};
+    playerInfo = ()=>{
+        return {x:this.x,y:this.y,colour:this.colour,health:this.health,ability:this.ability,cheat:this.cheat};
     }
 
-    const getAbility = (newAbility)=>{
-        ability = newAbility;
+    getAbility = (newAbility)=>{
+        this.ability = newAbility;
         return;
     }
-
-    return {
-        moveUp:moveUp,
-        moveLeft:moveLeft,
-        moveRight:moveRight,
-        moveDown:moveDown,
-        reduceHealth:reduceHealth,
-        increaseHealth:increaseHealth,
-        playerInfo:playerInfo,
-        getAbility:getAbility
-    }
-
+    
 }
