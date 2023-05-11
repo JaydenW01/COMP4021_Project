4cbb6aa1f0814868d7c74d4ca9e35c86241fb855
Lok Chi Pang <lcpang@connect.ust.hk>
2023-05-11 19:59:46 +0800

    fixed yellow sprite

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -87,7 +87,7 @@ const Game = function (sprites) {
         }
     };
 
-    updateBoard = function (gameBoard) {
+    updateBoard = function (gameBoard, repeat = true) {
         const now = new Date().getTime();
         lastGameBoard = JSON.parse(JSON.stringify(gameBoard));
         // console.log(breakables);
@@ -264,6 +264,9 @@ const Game = function (sprites) {
 
         console.log("fires list: "+JSON.stringify(fires));
 
+        if(repeat == true) {
+            setTimeout(updateBoard(lastGameBoard, false), 150);
+        }
     };
 
     explodeBomb = function (points) {
