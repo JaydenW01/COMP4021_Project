bcf1ffa3e0150ad26e3cab13fc677cb8665f083f
JaydenW01 <jaydenwong716hk@gmail.com>
2023-05-11 14:23:56 +0800

    gameboard can now be reset

--- server/src/server.js
+++ server/src/server.js
@@ -7,9 +7,56 @@ import { Server } from 'socket.io';
 import Lobby from './Lobby.js';
 import Gameboard from './Gameboard.js';
 import Player from './Player.js';
+const initialWalls = [
+    {x:1,y:1},
+    {x:3,y:1},
+    {x:5,y:1},
+    {x:7,y:1},
+    {x:9,y:1},
+    {x:11,y:1},
+    {x:1,y:3},
+    {x:3,y:3},
+    {x:5,y:3},
+    {x:7,y:3},
+    {x:9,y:3},
+    {x:11,y:3},
+    {x:1,y:5},
+    {x:3,y:5},
+    {x:5,y:5},
+    {x:7,y:5},
+    {x:9,y:5},
+    {x:11,y:5},
+    {x:1,y:7},
+    {x:3,y:7},
+    {x:5,y:7},
+    {x:7,y:7},
+    {x:9,y:7},
+    {x:11,y:7},
+    {x:1,y:9},
+    {x:3,y:9},
+    {x:5,y:9},
+    {x:7,y:9},
+    {x:9,y:9},
+    {x:11,y:9}
+];
+
+const initialBreakables = [
+    {x:2,y:0},{x:3,y:0},{x:4,y:0},{x:5,y:0},{x:6,y:0},{x:7,y:0},{x:8,y:0},{x:9,y:0},{x:10,y:0},{x:11,y:0},{x:12,y:0},
+    {x:2,y:1},{x:4,y:1},{x:6,y:1},{x:8,y:1},{x:10,y:1},{x:12,y:1},
+    {x:0,y:2},{x:1,y:2},{x:2,y:2},{x:3,y:2},{x:4,y:2},{x:5,y:2},{x:6,y:2},{x:7,y:2},{x:8,y:2},{x:9,y:2},{x:10,y:2},{x:11,y:2},{x:12,y:2},
+    {x:0,y:3},{x:2,y:3},{x:4,y:3},{x:6,y:3},{x:8,y:3},{x:10,y:3},{x:12,y:3},
+    {x:0,y:4},{x:1,y:4},{x:2,y:4},{x:3,y:4},{x:4,y:4},{x:5,y:4},{x:6,y:4},{x:7,y:4},{x:8,y:4},{x:9,y:4},{x:10,y:4},{x:11,y:4},{x:12,y:4},
+    {x:0,y:5},{x:2,y:5},{x:4,y:5},{x:6,y:5},{x:8,y:5},{x:10,y:5},{x:12,y:5},
+    {x:0,y:6},{x:1,y:6},{x:2,y:6},{x:3,y:6},{x:4,y:6},{x:5,y:6},{x:6,y:6},{x:7,y:6},{x:8,y:6},{x:9,y:6},{x:10,y:6},{x:11,y:6},{x:12,y:6},
+    {x:0,y:7},{x:2,y:7},{x:4,y:7},{x:6,y:7},{x:8,y:7},{x:10,y:7},{x:12,y:7},
+    {x:0,y:8},{x:1,y:8},{x:2,y:8},{x:3,y:8},{x:4,y:8},{x:5,y:8},{x:6,y:8},{x:7,y:8},{x:8,y:8},{x:9,y:8},{x:10,y:8},{x:11,y:8},{x:12,y:8},
+    {x:0,y:9},{x:2,y:9},{x:4,y:9},{x:6,y:9},{x:8,y:9},{x:10,y:9},
+    {x:0,y:10},{x:1,y:10},{x:2,y:10},{x:3,y:10},{x:4,y:10},{x:5,y:10},{x:6,y:10},{x:7,y:10},{x:8,y:10},{x:9,y:10},{x:10,y:10}
+];
+
 
 const lobby = new Lobby();
-let gameboard = null;
+let gameboard = new Gameboard(initialWalls,initialBreakables);
 const player1 = new Player(1);
 const player2 = new Player(2);
 
@@ -228,7 +275,6 @@ io.on("connection",(socket)=>{
                 hearts:gameboard.gameboardInfo().hearts,
                 coins:gameboard.gameboardInfo().coins
             }))},300);
-            gameboard = new Gameboard();
             io.emit("StartGame",JSON.stringify({players:[player1.playerInfo(),player2.playerInfo()],breakables:gameboard.gameboardInfo().breakables,bombs:gameboard.gameboardInfo().bombs,hearts:gameboard.gameboardInfo().hearts,fires:gameboard.gameboardInfo().fires}));
         } else if (result.result === "failed") {
             socket.emit("failed to ready",result.message);
@@ -590,7 +636,8 @@ io.on("connection",(socket)=>{
         player1.reset();
         player2.reset();
         lobby.reset();
-        gameboard = null;
+        gameboard.reset(initialWalls,initialBreakables);
+        gameboard.setBreakables(initialBreakables);
     })
 
     socket.on("debug",()=>{
